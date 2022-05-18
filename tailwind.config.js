d6eeeba7719107c88e2860a2bd112505b34ed3f6
amiribnjaman <amiribnjaman@gmail.com>
2022-05-18 22:09:36 +0600

    Skill assessment todo app initialized

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#9F07D9",
+          "secondary": "#FFFFFF",
+          "accent": "##2d3436",
+          "neutral": "#3D4451",
+          "base-100": "#FFFFFF",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
